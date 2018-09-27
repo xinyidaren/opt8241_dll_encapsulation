8de79b83f5ba4d9c06791361cc2c01b4ca2d1f11
xinyidaren <578341243@qq.com>
2018-09-27 16:12:06 +0800

    添加变量

--- test/pycallclass.cpp
+++ test/pycallclass.cpp
@@ -6,13 +6,17 @@ class TestLib
     public:
         void display();
         void display(int a);
+    private:
+        int i;
 };
 void TestLib::display() {
     cout<<"First display"<<endl;
+    i = 100;
 }
  
 void TestLib::display(int a) {
     cout<<"Second display:"<<a<<endl;
+    cout<<"i:"<<i<<endl;
 }
 extern "C" {
     TestLib obj;
