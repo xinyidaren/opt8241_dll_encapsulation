cf2408d3e41f5be8b4bfa34bff1176e2656420c5
xinyidaren <578341243@qq.com>
2018-09-29 11:07:52 +0800

    添加list传入传出函数

--- test/pycallclass.cpp
+++ test/pycallclass.cpp
@@ -6,24 +6,87 @@ class TestLib
     public:
         void display();
         void display(int a);
+        void display(char* msg);
+        int sum(int x);
+        int add(int a,int b);
+        int sum_array(int array[], int num);
+        void modify_array(int array[], int num);
     private:
-        int i;
 };
-void TestLib::display() {
+void TestLib::display()
+{
     cout<<"First display"<<endl;
-    i = 100;
 }
  
-void TestLib::display(int a) {
+void TestLib::display(int a)
+{
     cout<<"Second display:"<<a<<endl;
-    cout<<"i:"<<i<<endl;
 }
-extern "C" {
+
+void TestLib::display(char* msg)
+{
+    cout<<"Third display:"<<msg<<endl;
+}
+
+int TestLib::sum(int x)
+{
+    int i, result=0;
+    for(i=0; i<=x; i++)
+    {
+        result+=i;
+    }
+    return result;
+};
+
+int TestLib::add(int a,int b)
+{
+    return a+b;
+}
+
+int TestLib::sum_array(int array[], int num)
+{
+    int i =0, sum = 0;
+    for(i=0; i<num; ++i)
+        sum += array[i];
+    return sum;
+}
+
+void TestLib::modify_array(int array[], int num)
+{
+    int i =0, sum = 0;
+    for(i=0; i<num; ++i)
+        array[i] *= 10;
+}
+extern "C"
+{
     TestLib obj;
-    void display() {
+    void display()
+    {
         obj.display();
-      }
-    void display_int() {
+    }
+    void display_int()
+    {
         obj.display(2);
-      }
+    }
+    void display_char()
+    {
+        char * c="display_char";
+        obj.display(c);
+    }
+    int sum()
+    {
+        obj.sum(100);
+    }
+    int add(int a,int b)
+    {
+       return obj.add(a,b);
+    }
+    int sum_array(int array[], int num)
+    {
+       return obj.sum_array(array,num);
+    }
+    void modify_array(int array[], int num)
+    {
+        obj.modify_array(array,num);
+    }
 }
--- test/pycallclass.py
+++ test/pycallclass.py
@@ -1,8 +1,22 @@
 import ctypes
+import numpy as np
 so = ctypes.cdll.LoadLibrary
 lib = so("./libpycallclass.so")
-print ('display()')
+print ('------------------display()---------------------')
 lib.display()
-print ('display(100)')
+print ('------------------display(100)------------------')
 lib.display_int(100)
+print ('------------------display_char()------------------')
+lib.display_char()
+print ('------------------sum()------------------')
+print (lib.sum())
+print ('------------------add(3,4)------------------')
+print (lib.add(3,4))
+print ('------------------sum_array(carray, len(pyarray))------------------')
 
+pyarray = [1,2,3,4,5,6,7,8]
+carray = (ctypes.c_int*len(pyarray))(*pyarray)
+print(lib.sum_array(carray, len(pyarray)))
+print ('------------------modify_array(carray, len(pyarray))------------------')
+lib.modify_array(carray, len(pyarray))
+print (np.array(carray))
